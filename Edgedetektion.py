75d7ca846627033c67df5d72d7108089f400431e
Nick021001 <73854162+Nick021001@users.noreply.github.com>
2024-02-13 17:45:50 +0100

    Add files via upload

---
+++ Edgedetektion.py
@@ -0,0 +1,17 @@
+import numpy as np
+import skiamge as ski
+
+class Edgedetektion():
+    def __init__(self, image):
+        self.image = image
+
+    def sobelFilter():
+
+
+    def cannyFilter():
+
+
+    def createBoundingBox():
+
+
+    def __repr__(self): 
---
+++ WebappEdgedetektion.py
@@ -0,0 +1,5 @@
+import streamlit as st
+from Edgedetektion import *
+
+st.title("This is our Web Application for testing our Edge Detektion Algrotithmen based on insects images ")
+
